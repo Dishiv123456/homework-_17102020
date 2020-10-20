0cbd6326f3cc68589125dc69b44bedf504840a05
primetesting7 <primetesting7@gmail.com>
2020-10-20 12:17:47 +0100

    final virsion

--- homework-_17102020/src/homeworkweeksix/ExerciseSeventeen.java
+++ homework-_17102020/src/homeworkweeksix/ExerciseSeventeen.java
@@ -1,7 +1,31 @@
 package homeworkweeksix;
 
+import java.util.Scanner;
+
 public class ExerciseSeventeen {
     /* Write a Java program to convert a decimal number to binary number.
        Input Data: Input a Decimal Number : 5
        Expected Output Binary number is: 101   */
+
+    public static void main(String[] args) {
+        decimalToBinary();
+    }
+    public static void decimalToBinary(){
+    int quot, i = 1, j;
+        int bin_num[] = new int[20];
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input a decimal number : ");
+        int dec_num = s.nextInt();
+        quot = dec_num;
+        while (quot != 0) {
+            bin_num[i++] = quot % 2;
+            quot=quot/2;
+        }
+        System.out.print("Binary number is: ");
+        for(j=i-1; j>0; j--)
+        {
+            System.out.print(bin_num[j]);
+        }
+        System.out.print("\n");
+    }
 }
--- homework-_17102020/src/homeworkweeksix/ExerciseSixteen.java
+++ homework-_17102020/src/homeworkweeksix/ExerciseSixteen.java
@@ -1,10 +1,42 @@
 package homeworkweeksix;
 
+import java.util.Scanner;
+
 public class ExerciseSixteen {
     /* Write a Java program to add two binary numbers.
        Input Data:
        Input first binary number: 10
        Input second binary number: 11   */
 
+    public static void main(String[] args) {
+        ExerciseSixteen obj = new ExerciseSixteen();
+        obj.additionOFTwoBinary();
+    }
 
+    public void additionOFTwoBinary() {
+        int i = 0, remainder = 0;
+        int[] sum = new int[20];
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input first binary number: ");
+        long binary1 = in.nextLong();
+        System.out.println("Input second binary number: ");
+        long binary2 = in.nextLong();
+
+        while (binary1 != 0 || binary2 != 0) {
+            sum[i++] = (int) ((binary1 % 10 + binary2 % 10 + remainder) % 2);
+            remainder = (int) ((binary1 % 10 + binary2 % 10 + remainder) / 2);
+            binary1 = binary1 / 10;
+            binary2 = binary2 / 10;
+        }
+        if (remainder != 0) {
+            sum[i++] = remainder;
+        }
+        --i;
+        System.out.println("Sum of two binary numbers: ");
+        while (i >= 0) {
+            System.out.print(sum[i--]);
+        }
+        System.out.print("\n");
+    }
 }
